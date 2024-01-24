66f8f6d78afdc600065f5b9ad3dbd1861b410faf
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-01-24 21:24:28 +0100

    feat: finish zip chapter

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,10 @@
+import { pipeline } from "node:stream/promises"
+import fs from "node:fs"
+import zlib from "node:zlib"
 const compress = async () => {
-    // Write your code here 
+    const name = "./files/fileToCompress.txt"
+    const to = "./files/archive.gz"
+    await pipeline(fs.createReadStream(name), zlib.createGzip(), fs.createWriteStream(to))
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,11 @@
+import { pipeline } from "node:stream/promises"
+import fs from "node:fs"
+import zlib from "node:zlib"
+
 const decompress = async () => {
-    // Write your code here 
+    const to = "./files/fileToCompress.txt"
+    const from = "./files/archive.gz"
+    await pipeline(fs.createReadStream(from), zlib.createUnzip(), fs.createWriteStream(to))
 };
 
 await decompress();
