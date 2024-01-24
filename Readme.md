a5b3c3a7da08789920735ae260b534f8dd4d08c9
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-01-24 22:03:45 +0100

    feat: complete wt chapter

--- Readme.md
+++ Readme.md
@@ -6,8 +6,8 @@
 - [x] Command line interface(src/cli)
 - [x] Modules(src/modules)
 - [x] Hash (src/hash)
-- [ ] Streams (src/streams)
-- [ ] Zlib (src/zip)
-- [ ] Worker Threads (src/wt)
+- [x] Streams (src/streams)
+- [x] Zlib (src/zip)
+- [x] Worker Threads (src/wt)
 - [ ] Child Processes (src/cp)
 ### Done by Romas Bitinas
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import os from "os"
+import { Worker, getEnvironmentData } from "worker_threads"
+
 const performCalculations = async () => {
     // Write your code here
+    return new Promise ((res, rej) => {
+    const cores = os.cpus().length;
+    let results = []
+    let finished = 0;
+    let length = 10 + cores;
+    for (let i = 0; i < length; i++) {
+        const worker = new Worker("./worker.js", { workerData: i})
+        worker.on('message', (result) => {
+            results.push({status: 'resolve', data: result})
+        });
+        worker.on('error', () => results.push({status: "error", data: null}))
+        worker.on("exit", () => {
+            finished += 1;
+            if (finished === length) {
+                console.log(results);
+                res(results);
+            }
+        })
+    }
+    })
 };
 
 await performCalculations();
